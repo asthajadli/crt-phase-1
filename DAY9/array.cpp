4dd8b163e87f5170d27c7a55e1ddad7f30978601
asthajadli <jadliastha@gmail.com>
2026-05-09 14:15:13 +0530

    day 9 code 1

---
+++ DAY9/array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int arr[5];
+
+for (int i = 1; i<5; i++){
+
+    cin>>arr[i];
+
+}
+
+for (int i=1; i<5; i++){
+
+    cout<<arr[i]<<" ";}
+
+    return 0;
+
+
+
+}
